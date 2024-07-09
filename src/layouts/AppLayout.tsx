33a2ef4f1f2965383b4161f24addc07282824ffa
Everth Paredes <epe0413@gmail.com>
2024-07-08 20:35:07 -0600

    Creating the Layout of the application

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import Logo from '../components/Logo';
 
 export default function AppLayout() {
     return (
         <>
-            <div>AppLayout</div>
+            <header className='bg-gray-800 py-3 shadow-lg'>
+                <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+                    <div className='w-24'>
+                        <Logo/>
+                    </div>
+                </div>
+            </header>
 
-            <Outlet/>
+            <section className='max-w-screen-2xl mx-auto mt-3 p-2'>
+                <Outlet/>
+            </section>
+
+            <footer className='py-3'>
+                <p className='text-center'>
+                    Todos los derechos reservados {new Date().getFullYear()}
+                </p>
+
+            </footer>
         </>
     )
 }
