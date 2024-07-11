a722c089ea68270b4ee0fd1f0235f9df9068be0d
Everth Paredes <epe0413@gmail.com>
2024-07-10 21:46:19 -0600

    Creating the File that will handle the API requests

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 import ProjectForm from "@/components/projects/ProjectForm"
-import { ProjectFormData } from "types"
+import { ProjectFormData } from "@/types/index"
+import { createProject } from "@/api/ProjectAPI"
 
 export default function CreateProjectView() {
     const initialValues: ProjectFormData ={
@@ -13,7 +14,7 @@ export default function CreateProjectView() {
     const{register, handleSubmit, formState: {errors}} = useForm({defaultValues:initialValues })
 
     const handleForm = (data : ProjectFormData) => {
-        console.log(data)
+        createProject(data)
     }
     return (
         <>
