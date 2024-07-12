9f2f81f44ab076951ab070bf34f8f0d282ddd6ad
Everth Paredes <epe0413@gmail.com>
2024-07-11 18:08:37 -0600

    Displaying a Toast notification if the project is created successfully

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
 import ProjectForm from "@/components/projects/ProjectForm"
 import { ProjectFormData } from "@/types/index"
 import { createProject } from "@/api/ProjectAPI"
@@ -14,8 +15,9 @@ export default function CreateProjectView() {
 
     const{register, handleSubmit, formState: {errors}} = useForm({defaultValues:initialValues })
 
-    const handleForm = async(data : ProjectFormData) => {
-        await createProject(data)
+    const handleForm = async(formData : ProjectFormData) => {
+        const data = await createProject(formData)
+        toast.success(data)
         navigate('/')
     }
     return (
