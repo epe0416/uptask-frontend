b135973cc23801e765f1389b635d4f81b22ffe90
Everth Paredes <epe0413@gmail.com>
2024-07-19 20:29:44 -0600

    Get the Project to edit

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,11 +1,20 @@
 import { useParams } from "react-router-dom"
+import { useQuery} from "@tanstack/react-query"
+import { getProjectById } from "@/api/ProjectAPI"
 
 export default function EditProjectView() {
 
     const params = useParams()
     const projectId = params.projectId!
 
-    console.log(projectId)
+    const { data, isLoading } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+
+    console.log(data)
+    console.log(isLoading)
 
     return (
         <div>EditProjectView</div>
