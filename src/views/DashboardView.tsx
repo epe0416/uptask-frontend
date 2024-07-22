1daf47061d1805071bcd0aa5a74148fee8af1f0c
Everth Paredes <epe0413@gmail.com>
2024-07-21 18:59:10 -0600

    Removing Projects with useMutation

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -2,14 +2,26 @@ import { Link } from "react-router-dom"
 import { Fragment } from 'react'
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { useQuery } from '@tanstack/react-query'
-import { getProjects } from "@/api/ProjectAPI"
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteProject, getProjects } from "@/api/ProjectAPI"
+import { toast } from "react-toastify"
 
 export default function DashboardView() {
     
     const { data, isLoading } = useQuery({
         queryKey: ['projects'],
         queryFn: getProjects
+    })
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: deleteProject,
+        onError:(error) => {
+            toast.error(error.message)
+        },
+        onSuccess:(data) => {
+            queryClient.invalidateQueries({queryKey: ['projects']})
+            toast.success(data)
+        },
     })
     
     if(isLoading) return 'Cargando...'
@@ -74,7 +86,7 @@ export default function DashboardView() {
                                     <button
                                         type='button'
                                         className='block px-3 py-1 text-sm leading-6 text-red-500 hover:bg-slate-200 w-full text-start'
-                                        onClick={() => { }}
+                                        onClick={() => mutate(project._id)}
                                     >
                                         Eliminar Proyecto
                                     </button>
