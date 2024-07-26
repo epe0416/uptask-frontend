341519f35f7d925f9e7a59a3b07e1c059e88bb0d
Everth Paredes <epe0413@gmail.com>
2024-07-25 20:16:01 -0600

    Editing Tasks

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom"
+import { Navigate, useLocation, useParams } from "react-router-dom"
 import { useQuery } from '@tanstack/react-query'
 import { getTaskById } from "@/api/TaskAPI"
 import EditTaskModal from "./EditTaskModal"
@@ -12,13 +12,14 @@ export default function EditTaskData() {
     const queryParams = new URLSearchParams(location.search)
     const taskId = queryParams.get('editTask')!
     
-    const { data } = useQuery({
+    const { data, isError } = useQuery({
         queryKey: ['task', taskId],
         queryFn: () => getTaskById({projectId, taskId}),
-        enabled: !!taskId
+        enabled: !!taskId,
+        retry: false
     })
 
-    console.log(data)
+    if(isError) return <Navigate to={'/404'}/>
 
-    if(data) return <EditTaskModal data={data}/>
+    if(data) return <EditTaskModal data={data} taskId = {taskId}/>
 }
