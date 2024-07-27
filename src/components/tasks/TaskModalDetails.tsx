9253d4fbf8a8f850db7b67afdcebda40b3841e98
Everth Paredes <epe0413@gmail.com>
2024-07-26 20:45:39 -0600

    Validation that the tasks exist

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,15 +1,45 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getTaskById } from '@/api/TaskAPI';
+import { toast } from 'react-toastify';
 
 export default function TaskModalDetails() {
 
+    const [errorTask, setErrorTask] = useState(false)
+
+    const params = useParams()
+    const projectId = params.projectId!
     const navigate = useNavigate()
     const location = useLocation()
     const queryParams = new URLSearchParams(location.search)
-    const taskId = queryParams.get('viewTask')
+    const taskId = queryParams.get('viewTask')!
     
     const show = taskId ? true : false
+
+    const {data, isError, error} = useQuery({
+        queryKey: ['task', taskId],
+        queryFn: () => getTaskById({projectId, taskId}),
+        enabled: !!taskId,
+        retry: false
+    })
+
+    useEffect(() => {
+        function errorNavigate(){
+            if(isError){
+                toast.error(error.message, {toastId: 'error'})
+                setErrorTask(true)
+            }
+        }
+        errorNavigate()
+    },[isError, projectId, error, errorTask])
+
+    if(errorTask && isError){
+        return <Navigate to={`/projects/${projectId}`}/>
+    }
+
+    console.log(data)
 
     return (
         <>
