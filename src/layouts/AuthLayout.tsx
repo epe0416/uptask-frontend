1e9cfc3217dbc37f73751e21be6dd8b1989d5ee5
Everth Paredes <epe0413@gmail.com>
2024-08-08 20:22:22 -0600

    Creating the Login View and Zod Schema for the Form

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,12 +4,16 @@ import { Outlet } from 'react-router-dom'
 export default function AuthLayout() {
     return (
         <>
-            <div className='bg-gray-800 min-h-screen'>
+            <div className='bg-gray-800 min-h-screen flex flex-col items-center'>
                 <div className='py-10 lg:py-20 mx-auto w-[450px]'>
-                    <Logo/>
+                    <div className='w-60 mx-auto'>
+                        <Logo/>
+                    </div>
                     
-                    <div className='mt-10'>
-                        <Outlet/>
+                    <div className='mt-10 w-full flex justify-center'>
+                        <div className='w-[450px]'>
+                            <Outlet/>
+                        </div>
 
                     </div>
                 </div>
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,6 +1,71 @@
+import { useForm } from "react-hook-form";
+import { UserLoginForm } from "@/types/index";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function LoginView() {
+
+    const initialValues: UserLoginForm = {
+        email: '',
+        password: '',
+    }
+    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+    const handleLogin = (formData: UserLoginForm) => { }
+
     return (
-        <div>LoginView</div>
+        <>
+            <form
+                onSubmit={handleSubmit(handleLogin)}
+                className="space-y-2 p-8 bg-white rounded-lg"
+                noValidate
+            >
+                <div className="flex flex-col gap-5">
+                <label
+                    className="text-sm uppercase font-bold"
+                >Email</label>
+
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Email de Registro"
+                    className="w-full p-3 rounded-md border border-gray-200"
+                    {...register("email", {
+                    required: "El Email es obligatorio",
+                    pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: "E-mail no válido",
+                    },
+                    })}
+                />
+                {errors.email && (
+                    <ErrorMessage>{errors.email.message}</ErrorMessage>
+                )}
+                </div>
+
+                <div className="flex flex-col gap-5">
+                <label
+                    className="text-sm uppercase font-bold"
+                >Password</label>
+
+                <input
+                    type="password"
+                    placeholder="Password de Registro"
+                    className="w-full p-3 rounded-md border border-gray-200"
+                    {...register("password", {
+                    required: "El Password es obligatorio",
+                    })}
+                />
+                {errors.password && (
+                    <ErrorMessage>{errors.password.message}</ErrorMessage>
+                )}
+                </div>
+
+                <input
+                type="submit"
+                value='Iniciar Sesión'
+                className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white font-bold cursor-pointer transition-colors rounded-lg"
+                />
+            </form>
+        </>
     )
 }
