e82d76b8432806ccaec71b24fa0bb481861f5234
Everth Paredes <epe0413@gmail.com>
2024-08-13 10:47:08 -0600

    Creating the Account Confirmation View

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -6,12 +6,12 @@ export default function AuthLayout() {
     return (
         <>
             <div className='bg-gray-800 min-h-screen flex flex-col items-center'>
-                <div className='py-10 lg:py-20 mx-auto w-[450px]'>
+                <div className='py-5 lg:py-5 mx-auto w-[450px]'>
                     <div className='w-60 mx-auto'>
                         <Logo/>
                     </div>
                     
-                    <div className='mt-10 w-full flex justify-center'>
+                    <div className='mt-5 w-full flex justify-center'>
                         <div className='w-[450px]'>
                             <Outlet/>
                         </div>
