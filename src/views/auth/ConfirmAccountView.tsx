29bcfd028bb356d0226d57fdd56b3a494911b4c3
Everth Paredes <epe0413@gmail.com>
2024-08-13 11:21:48 -0600

    Adding a Component for Token Entry

--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { ConfirmToken } from "@/types/index";
 
 export default function ConfirmAccountView() {
 
+    const [token, setToken] = useState<ConfirmToken['token']>('')
+
+    const handleChange = (token: ConfirmToken['token']) => {
+        setToken(token)
+    }
+
+    const handleComplete = (token: ConfirmToken['token']) => {
+        console.log(token)
+    }
 
     return (
         <>
@@ -16,6 +28,16 @@ export default function ConfirmAccountView() {
             <label
             className="font-normal text-xl text-center block"
             >Código de 6 dígitos</label>
+            <div className="flex justify-center gap-5">
+                <PinInput value={token} onChange={handleChange} onComplete={handleComplete}>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                    <PinInputField className="w-10 h-10 p-3 rounded-lg border-gray-300 border placeholder-white text-center font-bold"/>
+                </PinInput>
+            </div>
 
         </form>
 
