ccdd4fa0adb9ec0cf2018148d54eee07930d8141
Everth Paredes <epe0413@gmail.com>
2024-08-16 17:10:37 -0600

    Validating the token in the frontend

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,9 +1,10 @@
 import NewPasswordToken from '@/components/auth/NewPasswordToken';
 import { useState } from 'react';
 import NewPasswordForm from '../../components/auth/NewPasswordForm';
+import { ConfirmToken } from '@/types/index';
 
 export default function NewPasswordView() {
-
+    const [token, setToken] = useState<ConfirmToken['token']>('')
     const [isValidToken, setIsValidToken] = useState(false)
     return (
         <>
@@ -13,7 +14,9 @@ export default function NewPasswordView() {
                 <span className=" text-fuchsia-500 font-bold"> en tu Email</span>
             </p>
 
-            {!isValidToken ? <NewPasswordToken/> : <NewPasswordForm/> }
+            {!isValidToken 
+                ? <NewPasswordToken token = {token} setToken={setToken} setIsValidToken={setIsValidToken}/> 
+                : <NewPasswordForm/> }
         </>
     )
 }
