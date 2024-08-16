b5a71f136d915d452c7c615fa093ba193d986f9b
Everth Paredes <epe0413@gmail.com>
2024-08-16 11:53:09 -0600

    Defining the New Password Reset Route

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,6 +1,19 @@
+import NewPasswordToken from '@/components/auth/NewPasswordToken';
+import { useState } from 'react';
+import NewPasswordForm from '../../components/auth/NewPasswordForm';
 
 export default function NewPasswordView() {
+
+    const [isValidToken, setIsValidToken] = useState(false)
     return (
-        <div>NewPasswordView</div>
+        <>
+            <h1 className="text-2xl font-black text-white">Reestablecer Password</h1>
+            <p className="text-xl font-light text-white mt-1">
+                Ingresa el código que recibiste  {''}
+                <span className=" text-fuchsia-500 font-bold"> en tu Email</span>
+            </p>
+
+            {!isValidToken ? <NewPasswordToken/> : <NewPasswordForm/> }
+        </>
     )
 }
