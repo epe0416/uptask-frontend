9cef4d9218895aa790b708903117c3c1054d4dc6
Everth Paredes <epe0413@gmail.com>
2024-08-22 10:57:09 -0600

    Redirect at login

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { UserLoginForm } from "@/types/index";
@@ -14,13 +14,15 @@ export default function LoginView() {
     }
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
+    const navigate = useNavigate()
+
     const {mutate} = useMutation({
         mutationFn: authenticateUser,
         onError: (error) => {
             toast.error(error.message)
         },
         onSuccess: () => {
-            toast.success('Iniciando Sesión')
+            navigate('/')
         }
     })
 
