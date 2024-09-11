b2bedebc87742e85cbb543b9770e29bcb38b332d
Everth Paredes <epe0413@gmail.com>
2024-09-10 21:37:20 -0600

    Showing search results

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -4,6 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../ErrorMessage";
 import { TeamMemberForm } from "@/types/index";
 import { findUserByEmail } from "@/api/TeamAPI";
+import SearchResult from "./SearchResult";
 
 export default function AddMemberForm() {
     const initialValues: TeamMemberForm = {
@@ -64,7 +65,7 @@ export default function AddMemberForm() {
             <div className="mt-5">
                 {mutation.isPending && <p className="text-center ">Cargando...</p>}
                 {mutation.error && <p className="text-center">{mutation.error.message}</p>}
-
+                {mutation.data && <SearchResult user={mutation.data}/>}
             </div>
         </>
     )
