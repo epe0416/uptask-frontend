1aa8e1da5d5c01e519d0a7e80378890f718eacfa
Everth Paredes <epe0413@gmail.com>
2024-10-28 10:38:36 -0600

    Removing notes and invalidating queries

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,7 +1,11 @@
+import { deleteNote } from "@/api/NoteAPI"
 import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useMemo } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type NoteDetailProps = {
     note: Note
@@ -11,6 +15,23 @@ export default function NoteDetail({note}: NoteDetailProps) {
 
     const {data, isLoading} = useAuth()
     const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+    const params = useParams()
+
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const taskId = queryParams.get('viewTask')!
+
+    const projectId = params.projectId!
+
+    const queryClient = useQueryClient()
+    const {mutate} = useMutation ({
+        mutationFn: deleteNote,
+        onError: (error) => toast.error(error.message),
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey: ['task', taskId]})
+        }
+    })
 
     if(isLoading) return 'Cargando...'
 
@@ -28,7 +49,11 @@ export default function NoteDetail({note}: NoteDetailProps) {
                 </p>
             </div>
             {canDelete && (
-                <button type="button" className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-md">Eliminar</button>
+                <button
+                    type="button"
+                    className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-md"
+                    onClick={() => mutate({projectId, taskId, noteId: note._id})}
+                >Eliminar</button>
             )}
         </div>
     )
