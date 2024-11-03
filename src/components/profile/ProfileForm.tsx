629893bba3e5dcc3d58c2d8bcdc4b5801fb72b02
Everth Paredes <epe0413@gmail.com>
2024-11-02 21:21:22 -0600

    Creating the Profile Form

---
+++ src/components/profile/ProfileForm.tsx
@@ -0,0 +1,77 @@
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../ErrorMessage"
+import { User, UserProfileForm } from "@/types/index"
+
+type ProfileFormProps = {
+    data: User
+}
+
+export default function ProfileForm({ data }: ProfileFormProps) {
+    const { register, handleSubmit, formState: { errors } } = useForm<UserProfileForm>({ defaultValues: data })
+
+    const handleEditProfile = (formData: UserProfileForm) => {}
+
+    return (
+        <>
+            <div className="mx-auto max-w-2xl">
+                <h1 className="text-xl font-black ">Mi Perfil</h1>
+                <p className="font-light text-gray-500 mt-1">Aquí puedes actualizar tu información</p>
+
+                <form
+                    onSubmit={handleSubmit(handleEditProfile)}
+                    className="mt-5 space-y-5  bg-white shadow-lg p-10 rounded-lg"
+                    noValidate
+                >
+                    <div className="mb-5 space-y-3">
+                        <label
+                            className="text-sm font-bold"
+                            htmlFor="name"
+                        >Nombre</label>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="Tu Nombre"
+                            className="w-full p-3 rounded-md border border-gray-200"
+                            {...register("name", {
+                                required: "Nombre de usuario es obligatoro",
+                            })}
+                        />
+                        {errors.name && (
+                            <ErrorMessage>{errors.name.message}</ErrorMessage>
+                        )}
+                    </div>
+
+                    <div className="mb-5 space-y-3">
+                        <label
+                            className="text-sm font-bold"
+                            htmlFor="password"
+                        >E-mail</label>
+                        <input
+                            id="text"
+                            type="email"
+                            placeholder="Tu Email"
+                            className="w-full p-3 rounded-md border border-gray-200"
+                            {...register("email", {
+                                required: "EL e-mail es obligatorio",
+                                pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "E-mail no válido",
+                                },
+                            })}
+                        />
+                        {errors.email && (
+                            <ErrorMessage>{errors.email.message}</ErrorMessage>
+                        )}
+                    </div>
+                    <div className="w-full flex justify-center">
+                        <input
+                            type="submit"
+                            value='Guardar Cambios'
+                            className="border border-fuchsia-600 hover:bg-fuchsia-700 text-gray-700 p-3 hover:text-white font-bold cursor-pointer transition-colors rounded-lg"
+                        />
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
