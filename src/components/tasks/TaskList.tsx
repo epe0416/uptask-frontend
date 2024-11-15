ddb08c27d8aa28acae6524a3184d34d62607bc59
Everth Paredes <epe0413@gmail.com>
2024-11-14 22:58:37 -0600

    Sending the request to the backend when we release

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,8 +1,12 @@
 import { DndContext, DragEndEvent } from "@dnd-kit/core"
-import { Task } from "@/types/index"
+import { Task, TaskStatus } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTraslation } from "@/locales/es"
 import DropTask from "./DropTask"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { updateStatus } from "@/api/TaskAPI"
+import { toast } from "react-toastify"
+import { useParams } from "react-router-dom"
 
 type TaskListProps = {
     tasks: Task[],
@@ -29,6 +33,22 @@ const statusStyle : {[key: string]: string}={
 }
 
 export default function TaskList({tasks, canEdit}: TaskListProps) {
+
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: updateStatus,
+        onError:(error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey: ['project', projectId]})
+            // queryClient.invalidateQueries({queryKey: ['task', taskId]})
+        }
+    })
     
     const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -38,8 +58,12 @@ export default function TaskList({tasks, canEdit}: TaskListProps) {
 
     const handleDragEnd = (e: DragEndEvent) => {
         const {over, active} = e
+        
         if(over && over.id) {
-            console.log('Valido..')
+            const taskId = active.id.toString()
+            const status = over.id as TaskStatus
+
+            mutate({projectId, taskId, status})
         }
     }
 
